36ac30241bb9fc047fa8031044299e1a46d126fc
Jule Blokinsky <jl.bls@atomicmail.io>
2026-03-07 22:22:44 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,8 +1,35 @@
-import esp
+"""The code is taken from (https://randomnerdtutorials.com/esp32-esp8266-micropython-web-server/)"""
 
-esp.osdebug(None)
 
-import webrepl
+try:
+  import usocket as socket    # usocket is MicroPython’s lightweight socket module
+except:
+  import socket               # If unavailable, it falls back to standard Python’s socket
 
-webrepl.start()
+from machine import Pin       # GPIO module 
+import network
 
+import esp                    # Disable ESP debug logs
+esp.osdebug(None)    
+
+import gc                     # Garbage collector to save RAM
+gc.collect()
+
+
+# ---- CONNECT TO WIFI ----
+ssid = 'REPLACE_WITH_YOUR_SSID'          
+password = 'REPLACE_WITH_YOUR_PASSWORD'
+
+station = network.WLAN(network.STA_IF)  # This connects your ESP to your home router
+
+station.active(True)        # active(True) turns Wi‑Fi on
+
+station.connect(ssid, password)  # Connects
+
+while station.isconnected() == False:  # This is a blocking loop, so nothing else runs until Wi‑Fi is ready.
+ pass
+
+print('Connection successful')
+print(station.ifconfig())  # (IP, subnet mask, gateway, DNS)
+
+led = Pin(2, Pin.OUT)  # GPIO 2 as an output
